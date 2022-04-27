91ad3c335d60183ff005b8fb4f1bc143ffd05dd4
krokhale <krishna.rokhale@gmail.com>
2022-04-26 18:01:42 -0700

    checking in

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+    const [allCategories, setAllCategories] = useState();
+    const [activeCategory, setActiveCategory] = useState();
+    const [questionFormText, setQuestionFormText] = useState('');
+
+
+    const fetchQuestionsForCategory = async () => {
+        let questions = await fetch(`http://localhost:3000/api/categories/${activeCategory.id}/questions`)
+        questions = await questions.json()
+        console.log('questions', questions)
+        // setAllCategories(categories)
+    };
+
+    useEffect(() => {
+        fetchQuestionsForCategory()
+    }, [activeCategory])
+
+    const fetchCategories = async () => {
+        let categories = await fetch('http://localhost:3000/api/categories')
+        categories = await categories.json()
+        setAllCategories(categories)
+    };
+
+    // run some code that runs once when the page loads up
+    useEffect(() => {
+        console.log('this code will run only once when the page loads up!')
+        fetchCategories()
+    }, [])
+
+    const onQuestionSubmit = async () => {
+        // POST request
+        let q = await fetch(`http://localhost:3000/api/categories/${activeCategory.id}/questions`, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({questionTxt: questionFormText})
+        })
+    };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>App</h1>
+
+        <h1>All Categories</h1>
+        <h1>Active Category: {JSON.stringify(activeCategory)}</h1>
+        <h1>questionFormText: {questionFormText}</h1>
+
+        {activeCategory && <>
+            <input type="text"
+                   value={questionFormText}
+                   onChange={(event) => setQuestionFormText(event.currentTarget.value)}
+                   placeholder={'Enter Question text'}/>
+            <button onClick={onQuestionSubmit}>Submit</button>
+        </>}
+
+        <ul>
+            {allCategories && allCategories.map((category, idx) => {
+                return <li key={idx} onClick={() => setActiveCategory(category)} >{category.name}</li>
+            })}
+        </ul>
+    </>
   );
 }
 
